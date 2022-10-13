48d5ae1f38ee5107c44ac8449a4232ee7b3b5919
Renny <reizka.deepone@gmail.com>
2022-10-13 17:53:24 +0200

    nav
    svelte navigation setup

---
+++ src/pages/Resume.svelte
@@ -0,0 +1,13 @@
+<main>
+    <div>
+    
+    </div>
+    <h1>RESUME</h1>
+  
+    <div class="card">
+   
+    </div>
+  
+  
+  
+</main>
---
+++ src/utility/navigation.svelte
@@ -0,0 +1,28 @@
+<script>
+    import { Router, Link, Route } from "svelte-navigator";
+	import About from "../pages/About.svelte";
+    import Resume from "../pages/Resume.svelte";
+	import Portfolio from "../pages/Portfolio.svelte";
+	import Contact from "../pages/Contact.svelte";
+    import Publications from "../pages/Publications.svelte";
+</script>
+
+<Router>
+	<nav>
+		<Link to="about">About</Link>
+		<Link to="resume">Resume</Link>
+        <Link to="portfolio">Portfolio</Link>
+		<Link to="contact">Contact</Link>
+        <Link to="publications">Publications</Link>
+	</nav>
+	<div>
+		<Route path="/">
+			<About />
+		</Route>
+		<Route path="about" component={About} />
+		<Route path="resume" component={Resume}/>
+		<Route path="portfolio" component={Portfolio}/>
+        <Route path="contact" component={Contact}/>
+        <Route path="publications" component={Publications}/>
+    </div>
+</Router>
