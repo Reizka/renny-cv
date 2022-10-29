e444248fb56cc67dbf3bb7988ad5a4bab81602f5
Nuuska Muikkunen <renny@rennys-Mac.local>
2022-10-29 10:02:07 +0200

    small changes to portfolio

--- src/routes/portfolio/portfolioComponent.svelte
+++ src/routes/portfolio/portfolioComponent.svelte
@@ -16,8 +16,8 @@
 </script>
 
 
-<div class="flex-col h-1/2 rounded-xl shadow-xl">
-    <div class="relative flex bg-white justify-center">
+<div class="flex-col h-1/2 rounded-xl ">
+    <div class="relative flex bg-black justify-center">
         {#if imageName === ""}
         <!-- svelte-ignore a11y-img-redundant-alt -->
         <img src={placeholder} alt="image placeholder" class="w-1/2"/>
@@ -27,10 +27,11 @@
             <img src={"$lib/images/"+{imageName}} alt="image of portfolio"/>
         
         {/if}
-        <h1 class="absolute bottom-0 left-1/2 -translate-x-1/2 border-3 bg-white p-2 pw-5">{title}</h1>
+        <div class="absolute w-full bottom-0 left-1/2  -translate-x-1/2 border-3 bg-gray-400 p-2 pw-5 opacity-50">
+            <h1 class="text-xl font-bold text-white opacity-100">{title}</h1>
+        </div>
     </div>
-    
-    <div class="flex-grow border-2 border-black rounded-xl shadow-xl bg-gray-400">
+    <div class="flex-grow border-2 h-[200px] border-black bg-gray-400 rounded-b-2xl shadow-2xl">
             <h2>{type}</h2>
             <p>{short_description}</p>
             <p>{keywords}</p>
