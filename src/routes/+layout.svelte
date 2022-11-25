931210cf372e45be68ab8cabf7c9c522085007bd
Nuuska Muikkunen <renny@rennys-Mac.local>
2022-11-25 17:04:24 +0100

    playing with styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,15 @@
 <script>
 	import Header from './Header.svelte';
 	import './styles.css';
-	import "./app.css";
+	import './app.css';
 </script>
 
 <div class="app">
 	<Header />
 
-<main>
-	<slot></slot>
-</main>
+	<main>
+		<slot />
+	</main>
 
 	<footer>
 		<p>footer</p>
@@ -22,18 +22,16 @@
 		flex-direction: column;
 		min-height: 100vh;
 	}
-
+	/*
 	main {
 		flex: 1;
 		display: flex;
 		flex-direction: column;
-		padding: 1rem;
 		width: 100%;
-		max-width: 64rem;
 		margin: 0 auto;
 		box-sizing: border-box;
 	}
-
+*/
 	footer {
 		display: flex;
 		flex-direction: column;
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -8,137 +8,155 @@
 	//https://v2.tailwindcss.com/docs/transform
 	//https://www.w3schools.com/Css/css3_2dtransforms.asp
 	//https://svelte.dev/tutorial/svelte-window-bindings
+
+	const tValue = (val) => {
+		return (-y * val) / 8;
+	};
 </script>
 
 <svelte:head>
 	<title>About</title>
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
+<svelte:window bind:scrollY={y} />
+<div id="wrapper" class="max-w-[100%]">
+	<h2 class="text-6xl relative postion-left-0 pb-10 underline ">About me</h2>
+	<div
+		class="text-container"
+		style="transform: translate(0,{(-y * layer) / (layers.length - 1)}px)"
+	>
+		<img
+			class="shadow-xl max-w-[20em] max-h-[20em] flex"
+			src="/main-portrait-cropped.jpg"
+			alt="main portrait"
+		/>
 
-<h2 class="text-6xl relative postion-left-0 pb-10 underline ">About me</h2>
+		<div class="flex p-6 m-2 border-solid ">
+			<div class="font-sans bg-white">
+				<p>
+					Hello and welcome to my homepage! This site is mainly intended to work as an online CV and
+					show off some of my old projects and know how.
+				</p>
 
-<div class="flex bg-white rounded-l shadow-2xl part-0 --tw-translate-y-,{0}">
-	<img
-		class="flex shadow-xl min-w-4/5 min-h-4/5"
-		src="/main-portrait-cropped.jpg"
-		alt="main portrait"
-	/>
+				<p>
+					It is not easy to nail down my exact are of focus in terms of employment as I mainly hail
+					from academia. My main passion though mostly revoles around games and game development.
+					But I have picked up some android and web development skills on the way as well
+				</p>
 
-	<div class="flex-column p-6 m-2 border-solid ">
-		<div class="font-sans bg-white">
-			<p>
-				Hello and welcome to my homepage! This site is mainly intended to work as an online CV and
-				show off some of my old projects and know how.
-			</p>
+				<p>
+					Before starting my studies I spent four years travelling around Ocenia, South-east Asia
+					and Israel. I worked in various different jobs during this time and during the second half
+					of my trip acquired my scuba-diving instructor license.
+				</p>
 
-			<p>
-				It is not easy to nail down my exact are of focus in terms of employment as I mainly hail
-				from academia. My main passion though mostly revoles around games and game development. But
-				I have picked up some android and web development skills on the way as well
-			</p>
-
-			<p>
-				Before starting my studies I spent four years travelling around Ocenia, South-east Asia and
-				Israel. I worked in various different jobs during this time and during the second half of my
-				trip acquired my scuba-diving instructor license.
-			</p>
-
-			<p>
-				After returning to Finland in 2012 I stared my Bacherlor studies at Haaga-Helia, where I
-				completed the Business Information Technology (BITe) in 2½ years (it is normally a 3½ year
-				program)
-			</p>
-			<p>
-				I continued directly to Msc studies after completing my Bachelor studies and moved to South
-				Korea for 2 years where I studied at Ajou University
-			</p>
-			<p>Lastly, I moved to Belgium to start my PhD, which I succesfully defended in July 2022.</p>
+				<p>
+					After returning to Finland in 2012 I stared my Bacherlor studies at Haaga-Helia, where I
+					completed the Business Information Technology (BITe) in 2½ years (it is normally a 3½ year
+					program)
+				</p>
+				<p>
+					I continued directly to Msc studies after completing my Bachelor studies and moved to
+					South Korea for 2 years where I studied at Ajou University
+				</p>
+				<p>
+					Lastly, I moved to Belgium to start my PhD, which I succesfully defended in July 2022.
+				</p>
+			</div>
 		</div>
 	</div>
-</div>
-<h2 class="text-6xl relative postion-left-0 pb-10 underline mt-20">My life</h2>
-<svelte:window bind:scrollY={y} />
+	<!--	text-container
+	justify-center  
+	relative
+	flex
+	flex-col
 
-<div class="grid place-items-center">
-	<div class="part-1  --tw-translate-y-{1}, {css}">
-		<h1>1986: The beginning</h1>
-		<h3>Several historical events happened that year</h3>
-		<ul>
-			<li>Chernobyl</li>
-			<li>Space shuttle Challenger</li>
-			<li>Halley's Comet</li>
-			<li>Mad cow disease</li>
-			<li>Me</li>
-		</ul>
-	</div>
-	<!--
-	<div class="part-2 ,{css}">
-		<h1>2006:Upper Secondary School</h1>
-		<p>
-			Graduated from upper Secondary school (Lukio) with grades that weren't bad, but also not great
-			enough to flaunt here separately
-		</p>
-	</div>
+	sm:	-bottom-[19em]
+	md: -bottom-[19em]
+	lg:	-bottom-[19em]
+	xl:-bottom-[190em]
+	2xl:-bottom-[50em]-->
+	<div class="">
+		<h2 class="text-6xl relative postion-left-0 pb-10 underline mt-20 ">My life</h2>
+		<div class="mt-10 bg-blue-400" style="transform: translate(0,{(-y * 1) / 7}px)">
+			<h1>1986: The beginning</h1>
+			<h3>Several historical events happened that year</h3>
+			<ul>
+				<li>Chernobyl</li>
+				<li>Space shuttle Challenger</li>
+				<li>Halley's Comet</li>
+				<li>Mad cow disease</li>
+				<li>Me</li>
+			</ul>
+		</div>
 
-	<div class="part-3, {css}">
-		<h1>2006: Army</h1>
-		<p>
-			Spending 6 months in (and mostly around) forest areas and learning the concept "Hurry to wait"
-			thoroughly.
-		</p>
+		<div class="bg-red-400" style="transform: translate(0,{(-y * 2) / 7}px)">
+			<h1>2006:Upper Secondary School</h1>
+			<p>
+				Graduated from upper Secondary school (Lukio) with grades that weren't bad, but also not
+				great enough to flaunt here separately
+			</p>
+		</div>
+
+		<div class="bg-amber-400" style="transform: translate(0,{(-y * 3) / 7}px)">
+			<p>
+				Spending 6 months in (and mostly around) forest areas and learning the concept "Hurry to
+				wait" thoroughly.
+			</p>
+		</div>
+		<div class="bg-green-400" style="transform: translate(0,{(-y * 4) / 7}px)">
+			<h1>2008 - 2011: Travelling</h1>
+			<p>
+				Spent nearly four years travelling Oceania and South-east Asia. I started off with a working
+				holiday in Australia, before picking up a scuba certificate in Thailand and later finding
+				work in Vietnam. Before coming back home I went to check out my roots in Israel
+			</p>
+		</div>
+		<div class="bg-blue-400" style="transform: translate(0,{(-y * 5) / 7}px)">
+			<h1>2012 - 2014: Haaga-Helia</h1>
+			<p>
+				Comeleted my studies in Business Information Tech, a degree program intended to teach both
+				business and programming with the idea of:"Jack of all trades, master of none"
+			</p>
+			<p>
+				While at Haaga-Helia, I also took on the head tutor activities. My Thesis focused on
+				creating an Eductional AR game for the Android device. The project was fun and I learned a
+				lot, but I am somewhat relieved no one will ever get their hands on the source code
+			</p>
+		</div>
+		<div class="bg-cyan-400" style="transform: translate(0,{(-y * 6) / 7}px)">
+			<h1>2015 - 2017: Ajou Univeristy South Korea</h1>
+			<p>
+				After finishing my studies I wanted to continue further with my studies and looking into
+				educational games. I was very lucky to get a position at Ajou university
+			</p>
+			<p>
+				I finished my Msc studies in time this time creating an adaptive educational game that
+				adapted slightly depending on the users' learning and play style. Again a very fun project
+				created using Unity3D and Springboot (backend) but equally happy the source code is lost to
+				sands of time
+			</p>
+		</div>
+		<div class="bg-pink-400" style="transform: translate(0,{(-y * 7) / 7}px)">
+			<h1>2017 - 2022: VUB Belgium</h1>
+			<p>
+				After my Msc, I was not ready to start real work and instead had yet another very lucky
+				chance as I was invited to do a PhD at Vrije Universiteit Brussel. I had also been invited
+				for a Phd at a Finnish University but the chance to live overseas was far too oppoturnity to
+				pass
+			</p>
+			<p>
+				This time my project was more convoluted and COVID did not help, but in the end I managed to
+				succesfully defend my thesis in July 2022 focusing on Social Engagement platforms and
+				creating designg recommendations for them with older users specifically in mind
+			</p>
+			<p>
+				The project involved number of smaller portions, such as creating a fully functional web
+				application on React & Firebase, as well as, using Unity3D to create a game that was
+				complimenting the project
+			</p>
+		</div>
 	</div>
-	<div class="part-4 , {css}">
-		<h1>2008 - 2011: Travelling</h1>
-		<p>
-			Spent nearly four years travelling Oceania and South-east Asia. I started off with a working
-			holiday in Australia, before picking up a scuba certificate in Thailand and later finding work
-			in Vietnam. Before coming back home I went to check out my roots in Israel
-		</p>
-	</div>
-	<div class="part-5 , {css}">
-		<h1>2012 - 2014: Haaga-Helia</h1>
-		<p>
-			Comeleted my studies in Business Information Tech, a degree program intended to teach both
-			business and programming with the idea of:"Jack of all trades, master of none"
-		</p>
-		<p>
-			While at Haaga-Helia, I also took on the head tutor activities. My Thesis focused on creating
-			an Eductional AR game for the Android device. The project was fun and I learned a lot, but I
-			am somewhat relieved no one will ever get their hands on the source code
-		</p>
-	</div>
-	<div class="part-6 , {css}">
-		<h1>2015 - 2017: Ajou Univeristy South Korea</h1>
-		<p>
-			After finishing my studies I wanted to continue further with my studies and looking into
-			educational games. I was very lucky to get a position at Ajou university
-		</p>
-		<p>
-			I finished my Msc studies in time this time creating an adaptive educational game that adapted
-			slightly depending on the users' learning and play style. Again a very fun project created
-			using Unity3D and Springboot (backend) but equally happy the source code is lost to sands of
-			time
-		</p>
-	</div>
-	<div class="part-7 , {css}">
-		<h1>2017 - 2022: VUB Belgium</h1>
-		<p>
-			After my Msc, I was not ready to start real work and instead had yet another very lucky chance
-			as I was invited to do a PhD at Vrije Universiteit Brussel. I had also been invited for a Phd
-			at a Finnish University but the chance to live overseas was far too oppoturnity to pass
-		</p>
-		<p>
-			This time my project was more convoluted and COVID did not help, but in the end I managed to
-			succesfully defend my thesis in July 2022 focusing on Social Engagement platforms and creating
-			designg recommendations for them with older users specifically in mind
-		</p>
-		<p>
-			The project involved number of smaller portions, such as creating a fully functional web
-			application on React & Firebase, as well as, using Unity3D to create a game that was
-			complimenting the project
-		</p>
-	</div>
--->
 </div>
 
 <style>
@@ -150,10 +168,9 @@
 	.textBox {
 		padding: 1em;
 	}
-	img {
-		max-width: 20em;
-		max-height: 20em;
-	}
-	.part-1 {
+
+	.text-container {
+		width: 100%;
+		background-color: blueviolet;
 	}
 </style>
