7a0c370ea781987e9848e0cb713dae622d517e5c
Renny <reizka.deepone@gmail.com>
2023-03-02 23:14:14 +0100

    netlify forms test

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -2,16 +2,18 @@
 	let name = '';
 	let email = '';
 	let message = '';
+	let sgAPI ="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"
 
 	function handleSubmit() {
-		// Verify the reCAPTCHA response
-		grecaptcha.ready(function () {
-			grecaptcha.execute('6LcB46AkAAAAAJe6LD9b22BcSy45z-wwkwtoWZyx', { action: 'login' }).then(function (token) {
-				// Send the email
-				// Replace this with your own email sending logic
-				console.log(`Name: ${name}`);
+		console.log(`Name: ${name}`);
 				console.log(`Email: ${email}`);
 				console.log(`Message: ${message}`);
+		// Verify the reCAPTCHA response
+		grecaptcha.enterprise.ready(function () {
+			grecaptcha.enterprise.execute(process.env.GOOGLE_RECAPTCHA, {action: 'login'}).then(function(token) {
+				// Send the email
+				// Replace this with your own email sending logic
+				
 				console.log(`reCAPTCHA response: ${token}`);
 
 				// Reset the form
@@ -25,26 +27,32 @@
 		return false;
 	}
 </script>
-<main><script src="https://www.google.com/recaptcha/enterprise.js?render=6LcB46AkAAAAAJe6LD9b22BcSy45z-wwkwtoWZyx" async defer></script>
+<main>
+
 <div class="flex flex-col items-center ">
 	<h2 class="text-6xl relative postion-left-0 pb-10 underline min-[400px]:text-2xl">Contact me</h2>
 	<div class="flex flex-col bg-white border-2 m-auto md:w-1/2 sm:w-[80%] shadow-2xl items-center">
-		<form on:submit|preventDefault={handleSubmit}>
-			<div class="flex flex-col mb-4">
-				<label for="name">Name</label>
-				<input class="border-solid border-2 border-black " type="text" id="name" bind:value={name} required />
+		<form name="contact" method="POST" data-netlify="true" netlify-honeypot="bot-field">
+			<p>
+			  <label for="name">Your Name:</label>
+			  <input type="text" name="name" id="name" required>
+			</p>
+			<p>
+			  <label for="email">Your Email:</label>
+			  <input type="email" name="email" id="email" required>
+			</p>
+			<p>
+			  <label for="message">Message:</label>
+			  <textarea name="message" id="message" required></textarea>
+			</p>
+			<div class="hidden">
+			  <label>Don’t fill this out if you're human: <input name="bot-field"></label>
 			</div>
-			<div class="flex flex-col mb-4">
-				<label for="email">Email</label>
-				<input class="border-solid border-2 border-black "  type="email" id="email" bind:value={email} required />
-			</div>
-			<div class="flex flex-col mb-4">
-				<label for="message">Message</label>
-				<textarea class="border-solid border-2 border-black "  id="message" bind:value={message} required />
-			</div>
-			<div class="g-recaptcha m-auto mb-4" data-sitekey="6LcB46AkAAAAAJe6LD9b22BcSy45z-wwkwtoWZyx" />
-			<button class="flex justify-center m-auto bg-blue-400 rounded-md font-bold mb-4 w-1/2 p-4" type="submit">Submit</button>
-		</form>
+			<p>
+			  <button type="submit">Send</button>
+			</p>
+		  </form>
+		  
 	</div>
 </div>
 </main>
