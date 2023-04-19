75e3a29a61c53c7b35d61f097b50bead2ab4e230
Nuuska Muikkunen <renny@rennys-Mac.local>
2023-04-19 17:12:22 +0200

    fixing the netlify form p2

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,9 @@ import adapter from '@sveltejs/adapter-auto';
 import preprocess from "svelte-preprocess";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	build: {
+		minify: true,
+	},
 	kit: {
 		adapter: adapter()
 	},
