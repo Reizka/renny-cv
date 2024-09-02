06fe8d223406f7792d56c0d19bd15cbb4c619dc6
Renny <reizka.deepone@gmail.com>
2024-09-02 14:19:21 +0200

    work-history page

--- src/routes/work-history/+page.svelte
+++ src/routes/work-history/+page.svelte
@@ -1,41 +1,47 @@
 <script>
     import resume from "$lib/data/resume.json";
-
-    // Function to generate a URL for the company logo based on the company name
-    function getCompanyLogoUrl(companyName) {
-        return `https://logo.clearbit.com/${companyName.toLowerCase().replace(/\s+/g, "")}.com`;
+    // Helper function to extract and format the date to Year and Month
+    function formatDate(dateString) {
+        const date = new Date(dateString);
+        const options = { year: "numeric", month: "short" };
+        return date.toLocaleDateString(undefined, options);
     }
 </script>
 
-<div class="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4">
-    {#each resume.work as job}
-        <div class="border p-4 rounded-lg shadow-lg">
-            <div class="flex items-center mb-4">
-                <img
-                    src={getCompanyLogoUrl(job.name)}
-                    alt="{job.name} logo"
-                    class="w-12 h-12 rounded-full mr-4 object-cover"
-                    onError={(e) => (e.target.style.display = "none")}
-                />
-                <div>
-                    <h3 class="text-lg font-bold">{job.position}</h3>
-                    <a
-                        href={job.url}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        class="text-blue-500 hover:underline"
-                    >
-                        {job.name}
-                    </a>
+<main>
+    <div class="flex flex-col justify-center items-center">
+        <h2 class="text-xl lg:text-6xl underline mt-4 min-[400px]:text-2xl">
+            Work-history
+        </h2>
+
+        <div class="grid grid-cols-1 gap-1 lg:w-2/3">
+            {#each resume.work as job}
+                <div class="border p-4 rounded-lg shadow-lg">
+                    <div class="flex items-center mb-4">
+                        <img
+                            src={"/images/" + job.image + ".png"}
+                            alt="{job.name} logo"
+                            class="w-12 h-12 rounded-full mr-4 object-cover"
+                            onError={(e) => (e.target.style.display = "none")}
+                        />
+                        <div>
+                            <h3 class="text-lg font-bold">{job.position}</h3>
+                            {job.name}
+                        </div>
+                    </div>
+                    <div class="text-gray-600">
+                        <p>{job.location}</p>
+                        <p>
+                            {formatDate(job.startDate)} - {formatDate(
+                                job.endDate,
+                            )}
+                        </p>
+                    </div>
+                    <div class="mt-2 text-gray-700">
+                        <p>{job.summary}</p>
+                    </div>
                 </div>
-            </div>
-            <div class="text-gray-600">
-                <p>{job.location}</p>
-                <p>{job.startDate} - {job.endDate}</p>
-            </div>
-            <div class="mt-2 text-gray-700">
-                <p>{job.summary}</p>
-            </div>
+            {/each}
         </div>
-    {/each}
-</div>
+    </div>
+</main>
